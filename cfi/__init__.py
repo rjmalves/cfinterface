9d8b5be389b6cb83d80d4a266d7480937bd757be
Rogerio Alves <rogerioalves.ee@gmail.com>
2022-03-31 14:43:45 -0300

    Initial setup

---
+++ cfi/__init__.py
@@ -0,0 +1,9 @@
+"""
+cfi
+====
+
+inewave is a Python module for handling custom formatted files
+and provide reading, storing and writing utilities.
+"""
+
+__version__ = "0.0.1"
---
+++ setup.py
@@ -0,0 +1,31 @@
+import cfi
+from setuptools import setup, find_packages  # type: ignore
+
+long_description = ""
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+requirements = []
+with open("requirements.txt", "r") as fh:
+    requirements = fh.readlines()
+
+setup(
+    name="cfi",
+    version=cfi.__version__,
+    author="Rogerio Alves",
+    author_email="rogerioalves.ee@gmail.com",
+    description="Interface for handling custom formatted files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rjmalves/cfi",
+    packages=find_packages(),
+    package_data={"cfi": ["py.typed"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+    ],
+    python_requires=">=3.5",
+    install_requires=requirements,
+)
