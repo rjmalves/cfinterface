254debf8c37a4fa5141869f49bbcdebfd244b1de
Rogerio Alves <rogerioalves.ee@gmail.com>
2022-04-04 09:20:11 -0300

    Testing writing

---
+++ tests/components/test_defaultblock.py
@@ -0,0 +1,42 @@
+from cfi.components.defaultblock import DefaultBlock
+from cfi.components.state import ComponentState
+from tests.mocks.mock_open import mock_open
+
+from unittest.mock import MagicMock, patch
+
+
+def test_default_block_eq():
+    b1 = DefaultBlock(data=0)
+    b2 = DefaultBlock(data=0)
+    assert b1 == b2
+    b1.data = 1
+    assert b1 != b2
+    b1.data = 0
+    b1.previous = b2
+    assert b1 != b2
+    b1.previous = None
+    b1.next = b2
+    assert b1 != b2
+
+
+def test_default_block_read():
+    data = "Hello, world!\n"
+    m: MagicMock = mock_open(read_data=data)
+    with patch("builtins.open", m):
+        with open("", "r") as fp:
+            b = DefaultBlock()
+            b.read_block(fp)
+            assert b.data == data
+            assert b.success
+
+
+def test_default_block_write():
+    data = "Hello, world!"
+    filedata = ""
+    m = mock_open(read_data=filedata)
+    with patch("builtins.open", m):
+        with open("", "w") as fp:
+            b = DefaultBlock(state=ComponentState.READ_SUCCESS)
+            b.data = data
+            b.write_block(fp)
+    m().write.assert_called_once_with(data)
---
+++ tests/writing/test_blockwriting.py
@@ -0,0 +1,43 @@
+from typing import IO, List
+
+from cfi.components.block import Block
+from cfi.data.blockdata import BlockData
+from cfi.writing.blockwriting import BlockWriting
+
+from tests.mocks.mock_open import mock_open
+
+from unittest.mock import MagicMock, patch
+
+
+class DummyBlock(Block):
+    BEGIN_PATTERN = "beg"
+    END_PATTERN = "end"
+
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, self.__class__):
+            return False
+        else:
+            return o.data == self.data
+
+    def read(self, file: IO) -> bool:
+        self.data: List[str] = []
+        while True:
+            line: str = file.readline()
+            self.data.append(line)
+            if self.ends(line):
+                break
+        return True
+
+    def write(self, file: IO) -> bool:
+        file.write(self.data)
+        return True
+
+
+def test_blockwriting_withdata():
+    filedata = "Hello, World!"
+    bd = BlockData(DummyBlock(data=filedata))
+    bw = BlockWriting(bd)
+    m: MagicMock = mock_open(read_data=filedata)
+    with patch("builtins.open", m):
+        bw.write("", "")
+    m().write.assert_called_once_with(filedata)
