9b161202bc633f56c0f9b082eac67144b66f762c
Rogerio Alves <rogerioalves.ee@gmail.com>
2023-07-31 09:47:27 -0300

    fix add_before and adds filters to data objects

--- tests/data/test_registerdata.py
+++ tests/data/test_registerdata.py
@@ -9,6 +9,10 @@ class DummyRegister(Register):
     IDENTIFIER_DIGITS = 4
     LINE = Line([LiteralField(13, 4)])
 
+    @property
+    def my_data(self):
+        return self.data[0]
+
 
 def test_registerdata_eq():
     rd1 = RegisterData(DummyRegister(data=-1))
@@ -50,6 +54,19 @@ def test_registerdata_add_before():
     rd.add_before(r1, r2)
     assert r1.previous == r2
     assert r2.next == r1
+    assert rd.last == r1
+    assert len(rd) == 3
+
+
+def test_registerdata_add_before_root():
+    r1 = DummyRegister(data=-1)
+    rd = RegisterData(r1)
+    r2 = DummyRegister(data=2)
+    rd.add_before(r1, r2)
+    assert r1.previous == r2
+    assert r2.next == r1
+    assert rd.last == r1
+    assert len(rd) == 2
 
 
 def test_registerdata_add_after():
@@ -60,6 +77,19 @@ def test_registerdata_add_after():
     rd.add_after(r1, r2)
     assert r1.next == r2
     assert r2.previous == r1
+    assert rd.last == r2
+    assert len(rd) == 3
+
+
+def test_registerdata_add_after_head():
+    r1 = DummyRegister(data=-1)
+    rd = RegisterData(r1)
+    r2 = DummyRegister(data=2)
+    rd.add_after(r1, r2)
+    assert r1.next == r2
+    assert r2.previous == r1
+    assert rd.last == r2
+    assert len(rd) == 2
 
 
 def test_registerdata_remove():
@@ -73,6 +103,22 @@ def test_registerdata_remove():
 
 def test_registerdata_of_type():
     rd = RegisterData(DummyRegister())
-    rd.append(Register())
+    rd.append(DummyRegister())
     assert len(rd) == 2
-    assert len([b for b in rd.of_type(DummyRegister)]) == 1
+    assert len([b for b in rd.of_type(DummyRegister)]) == 2
+
+
+def test_RegisterData_get_registers_of_type_no_filter():
+    r1 = DummyRegister(data=[10])
+    rd = RegisterData(r1)
+    rd.append(Register())
+    assert rd.get_registers_of_type(DummyRegister) == r1
+
+
+def test_RegisterData_get_registers_of_type_filter():
+    r1 = DummyRegister(data=[10])
+    rd = RegisterData(r1)
+    rd.append(DummyRegister())
+    rd.append(DummyRegister(data=[11]))
+    assert len(rd.get_registers_of_type(DummyRegister)) == 3
+    assert rd.get_registers_of_type(DummyRegister, my_data=10) == r1
