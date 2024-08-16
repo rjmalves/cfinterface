2b9b4e29a1791254f4a7380632436ce12302636b
Rogerio Alves <rogerioalves.ee@gmail.com>
2024-08-16 14:59:50 -0300

    fix setup

--- setup.py
+++ setup.py
@@ -9,9 +9,6 @@ requirements = []
 with open("requirements.txt", "r") as fh:
     requirements = fh.readlines()
 
-with open("dev-requirements.txt", "r") as fh:
-    extras_requirements = {"dev": fh.readlines()}
-
 
 setup(
     name="cfinterface",
@@ -32,5 +29,4 @@ setup(
     ],
     python_requires=">=3.8",
     install_requires=requirements,
-    extras_require=extras_requirements,
 )
